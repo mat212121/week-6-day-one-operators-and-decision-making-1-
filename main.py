97fb53378c140a98cd625b13e999b71d7b04e207
Mateo Chavez <113551958+mat212121@users.noreply.github.com>
2022-09-29 20:09:52 +0000

    loops

--- main.py
+++ main.py
@@ -33,117 +33,146 @@
 # == equal
 # != different or not equal to
 
-#learning comparison operators
-my_bool = 10 == 20 #true or false
-print(my_bool)
+# #learning comparison operators
+# my_bool = 10 == 20 #true or false
+# print(my_bool)
 
-my_bool = 5 + 5 == 10 - 0
-print(my_bool)
+# my_bool = 5 + 5 == 10 - 0
+# print(my_bool)
 
-my_bool = 'white' == 'White'.lower()
-print(my_bool)
+# my_bool = 'white' == 'White'.lower()
+# print(my_bool)
 
-my_bool = 4 < 5 and 5 > 6
-print(my_bool)
+# my_bool = 4 < 5 and 5 > 6
+# print(my_bool)
 
-# Comparison Operators Practice 1:
-# ask the user for their age
-# if the user's  is between 18 - 21 , print they can vote, other wise print better luck next time.
-age = int(input("what is your age? "))
+# # Comparison Operators Practice 1:
+# # ask the user for their age
+# # if the user's  is between 18 - 21 , print they can vote, other wise print better luck next time.
+# age = int(input("what is your age? "))
 
-if age >= 18 and age <=21:
-  print("you can vote")
-else:
-  print("better luck next time")
+# if age >= 18 and age <=21:
+#   print("you can vote")
+# else:
+#   print("better luck next time")
 
-# Comparison Operators Practice  2:
-# Create two variables (num1 and num2) with the following values: 36 and 17. Check if num1 is greater than or equal to num2 and store the result of that comparison in a variable called my_bool
-num1 = 36
-num2 = 17
-my_bool = num1 >= num2
-print(my_bool)
-
-
-# Comparison Operators Practice  3:
-# Create two variables (num1 and num2):
-# Inside num1, store the result of the square root of 25
-# Inside num2, store the number 5.
-# Check if num1 is equal to num2 and store the result of that comparison in a variable called my_bool.
-import math
-num1 = math.sqrt(25)
-num2 = 5
-my_bool = num1 >= num2
-print(my_bool)
-
-# Comparison Operators Practice #4:
-# Create two variables (num1 and num2):
-# Inside num1, store the result of 64 x 3
-num1 = 64*3
-# Inside num2, store the result of 24 x 8
-num2 = 24*8
-# Check if num1 is different from num2 and store the result of that comparison in a variable called my_bool.
-my_bool = num1 != num2
-print(my_bool)
+# # Comparison Operators Practice  2:
+# # Create two variables (num1 and num2) with the following values: 36 and 17. Check if num1 is greater than or equal to num2 and store the result of that comparison in a variable called my_bool
+# num1 = 36
+# num2 = 17
+# my_bool = num1 >= num2
+# print(my_bool)
 
 
-##############################logical operators##################################################
-# logical operators lesson
-my_bool = (55==10) and (5 == 2*3)
-print(my_bool)
+# # Comparison Operators Practice  3:
+# # Create two variables (num1 and num2):
+# # Inside num1, store the result of the square root of 25
+# # Inside num2, store the number 5.
+# # Check if num1 is equal to num2 and store the result of that comparison in a variable called my_bool.
+# import math
+# num1 = math.sqrt(25)
+# num2 = 5
+# my_bool = num1 >= num2
+# print(my_bool)
 
-my_bool = 1 == 10 or 3 == 3
-print(my_bool)
-
-text"this sentence is short"
-my_bool=('sentence' in text) and ('python' not in text)
-print(my_bool)
-
-# Logical Operators Practice #1
-# Create three variables (num1, num2, and num3):
-
-# Inside num1, store the value 36
-num1 = 36
-# Inside num2, stores the result of the operation 72/2
-num2 = 72/2
-# Inside num3, store the value 48
-num3 = 48
-# Check if num1 is greater than num2, and less than num3. Store the result of that comparison in a variable called my_bool.
-my_bool = num1 >= num2 and num1 <= num3
-print(my_bool)
+# # Comparison Operators Practice #4:
+# # Create two variables (num1 and num2):
+# # Inside num1, store the result of 64 x 3
+# num1 = 64*3
+# # Inside num2, store the result of 24 x 8
+# num2 = 24*8
+# # Check if num1 is different from num2 and store the result of that comparison in a variable called my_bool.
+# my_bool = num1 != num2
+# print(my_bool)
 
 
-# Logical Operators Practice #2
-# Create three variables (num1, num2, and num3):
+# ##############################logical operators##################################################
+# # logical operators lesson
+# my_bool = (55==10) and (5 == 2*3)
+# print(my_bool)
 
-# Inside num1, store the value 36
-num1 = 36
-# Inside num2, stores the result of the operation 72/2
-num2 = 
-# Inside num3, store the value 48
+# my_bool = 1 == 10 or 3 == 3
+# print(my_bool)
 
-# Check if num1 is greater than num2, or less than num3. Store the result of that comparison in a variable called my_bool.
+# text = "this sentence is short"
+# my_bool=('sentence' in text) and ('python' not in text)
+# print(my_bool)
+
+# # Logical Operators Practice #1
+# # Create three variables (num1, num2, and num3):
+
+# # Inside num1, store the value 36
+# num1 = 36
+# # Inside num2, stores the result of the operation 72/2
+# num2 = 72/2
+# # Inside num3, store the value 48
+# num3 = 48
+# # Check if num1 is greater than num2, and less than num3. Store the result of that comparison in a variable called my_bool.
+# my_bool = num1 >= num2 and num1 <= num3
+# print(my_bool)
 
 
+# # Logical Operators Practice #2
+# # Create three variables (num1, num2, and num3):
 
-# Logical Operators Practice #3
-# Check if the words:
+# # Inside num1, store the value 36
+# num1 = 36
+# # Inside num2, stores the result of the operation 72/2
+# num2 = 72/2
+# # Inside num3, store the value 48
+# num3 = 48
+# # Check if num1 is greater than num2, or less than num3. Store the result of that comparison in a variable called my_bool.
+# my_bool = num1 >= num2 or num1 <= num3
+# print(my_bool)
 
-# word1 = "success", and
 
-# word2 = "technology"
+# # Logical Operators Practice #3
+# # Check if the words:
 
-# are not found in the sentence below, and store the result (a boolean) in a variable called my_bool:
+# # word1 = "success", and
+# word1 = "success"
+# # word2 = "technology"
+# word2 = "tecnology"
+# # are not found in the sentence below, and store the result (a boolean) in a variable called my_bool:
 
-# "When something is important enough, you do it even if the odds are against you" - Elon Musk
-
-
+# # "When something is important enough, you do it even if the odds are against you" - Elon Musk
+# text = "When something is important enough, you do it even if the odds are against you"
 
 
 
 ########################################decision making in python#####################################
+pet = "dog"
+if pet == "cat":
+  print("you have a cat")
+elif pet == "dog":
+  print("you have a dog")
+else:
+  print("I don't know what animal you have")
+
+age  = 16
+schoolGrade = 9
+if age < 18:
+  print("you are a minor")
+  if schoolGrade >= 7:
+    print("passed")#nested if conditonals
+  else:
+    print('failed')
+else:
+  print("you are an adult")
 
 
+age = int(input("what is your age? "))
 
+if age < 20:
+  print("i am young")
+elif age < 30:
+  print("i am a vicenarian")
+elif age < 40:
+  print("i am a tricenarian")
+elif age < 50:
+  print("i am a quadragenarian")
+else:
+  print("i am old")
 # Decision Making Practice #1
 # Using the variables num1 and num2, which are fed with user input (just like in the provided code), create a flow control structure that compares the values of the variables, and returns a result according to the case:
 
@@ -154,8 +183,16 @@ num2 =
 # "num1 and num2 are equal"
 
 # You must display the value of the user input instead of num1 and num2.
-# num1 = input("Enter a number:")
-# num2 = input("Enter another number:")
+num1 = input("Enter a number:")
+num2 = input("Enter another number:")
+if num1 >= num2:
+  print(" the first number is greater")
+elif num1 <= num2:
+  print("the second number is greater")
+elif num1 == num2:
+  print('they are equal')
+else:
+  print("wake up")
 
 # f"{num1} is greater than {num2}"
 # "num2 is greater than num1"
@@ -166,6 +203,7 @@ num2 =
 # The laws of a certain country establish that an adult can drive if they are of legal age (18 years or older), and have a driver's license.
 
 # Create a conditional structure to check if a 16-year-old without a license can drive, and display the corresponding result on the screen:
+age = int(input("what is your age? "))
 
 # "You can drive"
 
@@ -231,8 +269,11 @@ knows_python = False
 # queue videos
 #what is iteration?
 #what are for loops?
-
-
+my_list = ['a','b','c','d']
+for letter in my_list:
+  letter_number = my_list.index(letter)+1
+  print(f"letter {letter_number}: {letter}")
+  
 # For Loops Practice #1
 # Using For loops, greet all members of a class, printing "Hello" + their name.
 
@@ -417,3 +458,46 @@ print(monthConversions)
 
 
 ##############################enumerators in python #####################################################
+# Enumerator Practice #1
+# Print sentences like the following on the screen:
+
+# '{name} is found at index {index}'
+
+# Where name must be each of the names in the list below, and the index, must be obtained via enumerate().
+
+# list_names = ["Steven", "Jackie", "Donna", "Kelso", "Eric", "Fez", "Kitty", "Red"]
+
+# You can use the given print() line as an example and change its variable names, but the returned phrases must be the same!
+
+# Tip: use loops!
+
+# list_names = ["Steven", "Jackie", "Donna", "Kelso", "Eric", "Fez", "Kitty", "Red"]
+
+# print(f'{nombre} se encuentra en el índice {indice}')
+
+
+
+# Enumerator Practice #2
+# Create a list formed by the tuples (index, element), obtained through enumerating the indices of each character of the "Python" string.
+
+# Call the returned list with the variable name indices_list.
+
+# "Python"
+
+
+# Enumerator Practice #3
+# Print to the screen only the indices of those names in the list below, that start with M:
+
+# list_names = ["Maverick", "Alice", "Madeline", "Hazel", "Jack", "Meadow", "Thomas", "Emily", "Mills"]
+
+# You can solve it in different ways, but it will help you keeping mind some (if not all) the following elements:
+
+# loops
+
+# if conditionals
+
+# the enumerate() method
+
+# string methods and indexing
+
+# list_names = ["Maverick", "Alice", "Madeline", "Hazel", "Jack", "Meadow", "Thomas", "Emily", "Mills"]
